a1d169dec0f81608966db1fe3b4648e1a4623867
esteban127 <30833442+esteban127@users.noreply.github.com>
2019-10-02 10:23:02 -0300

    added nav mesh

---
+++ EstebanRandom/Assets/NavController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavController : MonoBehaviour
+{
+    [SerializeField] Transform[] targets;
+    NavMeshAgent agent;
+    int currentTarget = 0;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(targets[0].position);
+    }
+    private void Update()
+    {
+        if ((transform.position - agent.destination).magnitude <= 1)
+        {
+            currentTarget++;
+            currentTarget = currentTarget % targets.Length;
+            agent.SetDestination(targets[currentTarget].position);
+        }
+    }
+}
